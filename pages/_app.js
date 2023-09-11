0c91787bbca330566d9fcff469b28c047c3fb6d4
ManuelDev <manueldeveloper17@gmail.com>
2023-09-10 23:32:37 -0500

    Feature: add transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
+import { useRouter } from "next/router";
+
 // Components
 import Layout from "../components/Layout";
+import Transition from "../components/Transition";
+
+// Motion
+import { AnimatePresence, motion } from "framer-motion";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
