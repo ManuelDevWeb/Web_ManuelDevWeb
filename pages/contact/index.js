a27a94760e9ef6b233438954a9d2e349ad99fbcb
ManuelDev <manueldeveloper17@gmail.com>
2023-09-14 12:21:33 -0500

    Feature: contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,56 @@
+// Components
+import Circles from "../../components/Circles";
+
+// Icons
+import { BsArrowRight } from "react-icons/bs";
+
+// Motion
+import { motion } from "framer-motion";
+
+// Variants
+import { fadeIn } from "../../variants";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="bg-primary/80 xl:bg-primary/60 h-full">
+      <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
+        <div className="flex flex-col w-full max-w-[700px] ">
+          {/* Text */}
+          <motion.h2
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2 text-center mb-12"
+          >
+            Let&apos;s <span className="text-yellow-500">connect.</span>{" "}
+          </motion.h2>
+          {/* Form */}
+          <motion.form
+            variants={fadeIn("up", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="flex-1 flex flex-col gap-6 w-full mx-auto"
+          >
+            {/* Input group */}
+            <div className="flex gap-x-6 w-full">
+              <input type="text" placeholder="name" className="input" />
+              <input type="email" placeholder="email" className="input" />
+            </div>
+            <input type="text" placeholder="subject" className="input" />
+            <textarea placeholder="message" className="textarea" />
+            <button className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-yellow-500 group">
+              <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
+                Let&apos;s talk
+              </span>
+              <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
+            </button>
+          </motion.form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- pages/work/index.js
+++ pages/work/index.js
@@ -16,7 +16,7 @@ const Work = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* Text */}
-          <div className="text-center flex flex-col xl:w-[30vw] lg:text-left mv-4 xl:mb-0">
+          <div className="text-center flex flex-col xl:w-[30vw] lg:text-left mb-4 xl:mb-0">
             <motion.h2
               variants={fadeIn("up", 0.2)}
               initial="hidden"
